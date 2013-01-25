f2f484035fbcba53ca9c914f3a303714b770de7c
John Leacox <johnlcox@gmail.com>
2013-01-24 21:19:42 -0600

    Added additional constructor and more javadoc

--- src/main/java/com/leacox/process/FinalizedProcessBuilder.java
+++ src/main/java/com/leacox/process/FinalizedProcessBuilder.java
@@ -2,6 +2,7 @@ package com.leacox.process;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * This class wraps {@link ProcessBuilder} for creating operating system
@@ -31,7 +32,24 @@ import java.io.IOException;
  * Starting a new process with the default attributes is just as easy as
  * {@link ProcessBuilder}:
  * 
+ * <pre>
  * {@code FinalizedProcess process = new ProcessBuilder("myCommand", "myArg").start();}
+ * </pre>
+ * 
+ * Here is an example that redirects standard error to standard output and does
+ * not destroy the process when the process is closed:
+ * 
+ * <pre>
+ * {@code
+ * FinalizedProcessBuilder pb = new FinalizedProcessBuilder("myCommand", "myArg");
+ * pb.redirectErrorStream(true);
+ * pb.keepProcess();
+ * FinalizedProcess process = pb.start();
+ * }
+ * </pre>
+ * 
+ * Note: For additional documentation see {@link ProcessBuilder}.
+ * <p>
  * 
  * @author John Leacox
  * 
@@ -41,8 +59,86 @@ public class FinalizedProcessBuilder {
 
 	private boolean keepProcess = false;
 
-	public FinalizedProcessBuilder(String... command) {
+	/**
+	 * Constructs a process builder with the specified operating system program
+	 * and arguments. This constructor does <i>not</i> make a copy of the
+	 * {@code command} list. Subsequent updates to the list will be reflected in
+	 * the state of the process builder. It is not checked whether
+	 * {@code command} corresponds to a valid operating system command.
+	 * 
+	 * @param command
+	 *            the list containing the program and its arguments (cannot be
+	 *            null)
+	 * @throws NullPointerException
+	 *             if command is null
+	 */
+	public FinalizedProcessBuilder(List<String> command) {
+		if (command == null) {
+			throw new NullPointerException();
+		}
 		this.processBuilder = new ProcessBuilder(command);
+	}
+
+	/**
+	 * Constructs a process builder with the specified operating system program
+	 * and arguments. This is a convenience constructor that sets the process
+	 * builder's command to a string list containing the same strings as the
+	 * command array, in the same order. It is not checked whether command
+	 * corresponds to a valid operating system command.
+	 * 
+	 * @param command
+	 *            a string array containing the program and its arguments
+	 *            (cannot be null)
+	 * @throws NullPointerException
+	 *             of command is null
+	 */
+	public FinalizedProcessBuilder(String... command) {
+		if (command == null) {
+			throw new NullPointerException("command: null");
+		}
+		this.processBuilder = new ProcessBuilder(command);
+	}
+
+	/**
+	 * Sets this process builder's operating system program and arguments. This
+	 * method does <i>not</i> make a copy of the {@code command} list.
+	 * Subsequent updates to the list will be reflected in the state of the
+	 * process builder. It is not checked whether {@code command} corresponds to
+	 * a valid operating system command.
+	 * 
+	 * @param command
+	 *            the list containing the program and its arguments (cannot be
+	 *            null)
+	 * @return this process builder
+	 * 
+	 * @throws NullPointerException
+	 *             if the command is null
+	 */
+	public FinalizedProcessBuilder command(List<String> command) {
+		processBuilder.command(command);
+		return this;
+	}
+
+	/**
+	 * Sets this process builder's operating system program and arguments. This
+	 * is a convenience method that sets the command to a string list containing
+	 * the same strings as the {@code command} array, in the same order. It is
+	 * not checked whether {@code command} corresponds to a valid operating
+	 * system command.
+	 * 
+	 * @param command
+	 *            a string array containing the program and its arguments
+	 *            (cannot be null)
+	 * @return this process builder
+	 * @throws NullPointerException
+	 *             if command is null
+	 */
+	public FinalizedProcessBuilder command(String... command) {
+		if (command == null) {
+			throw new IllegalArgumentException("command: null");
+		}
+		processBuilder.command(command);
+		return this;
 	}
 
 	public FinalizedProcessBuilder redirectErrorStream(boolean redirectErrorStream) {
@@ -76,6 +172,76 @@ public class FinalizedProcessBuilder {
 		return this;
 	}
 
+	/**
+	 * Starts a new process using the attributes of this process builder.
+	 * 
+	 * <p>
+	 * The new process will invoke the command and arguments given by
+	 * {@link #command()}, in a working directory as given by
+	 * {@link #directory()}, with a process environment as given by
+	 * {@link #environment()}.
+	 * 
+	 * <p>
+	 * This method checks that the command is a valid operating system command.
+	 * Which commands are valid is system-dependent, but at the very least the
+	 * command must be a non-empty list of non-null strings.
+	 * 
+	 * <p>
+	 * A minimal set of system dependent environment variables may be required
+	 * to start a process on some operating systems. As a result, the subprocess
+	 * may inherit additional environment variable settings beyond those in the
+	 * process builder's {@link #environment()}.
+	 * 
+	 * <p>
+	 * If there is a security manager, its {@link SecurityManager#checkExec
+	 * checkExec} method is called with the first component of this object's
+	 * {@code command} array as its argument. This may result in a
+	 * {@link SecurityException} being thrown.
+	 * 
+	 * <p>
+	 * Starting an operating system process is highly system-dependent. Among
+	 * the many things that can go wrong are:
+	 * <ul>
+	 * <li>The operating system program file was not found.
+	 * <li>Access to the program file was denied.
+	 * <li>The working directory does not exist.
+	 * </ul>
+	 * 
+	 * <p>
+	 * In such cases an exception will be thrown. The exact nature of the
+	 * exception is system-dependent, but it will always be a subclass of
+	 * {@link IOException}.
+	 * 
+	 * <p>
+	 * Subsequent modifications to this process builder will not affect the
+	 * returned {@link FinalizedProcess}.
+	 * 
+	 * @return a new {@link FinalizedProcess} object for managing the subprocess
+	 * @throws NullPointerException
+	 *             if an element of the command list is null *
+	 * @throws IndexOutOfBoundsException
+	 *             if the command is an empty list (has size {@code 0}) *
+	 * @throws SecurityException
+	 *             if a security manager exists and
+	 *             <ul>
+	 * 
+	 *             <li>its {@link SecurityManager#checkExec checkExec} method
+	 *             doesn't allow creation of the subprocess, or
+	 * 
+	 *             <li>the standard input to the subprocess was
+	 *             {@linkplain #redirectInput redirected from a file} and the
+	 *             security manager's {@link SecurityManager#checkRead
+	 *             checkRead} method denies read access to the file, or
+	 * 
+	 *             <li>the standard output or standard error of the subprocess
+	 *             was {@linkplain #redirectOutput redirected to a file} and the
+	 *             security manager's {@link SecurityManager#checkWrite
+	 *             checkWrite} method denies write access to the file
+	 * 
+	 *             </ul>
+	 * @throws IOException
+	 *             if an I/O error occurs
+	 */
 	public FinalizedProcess start() throws IOException {
 		Process process = processBuilder.start();
 		return new FinalizedProcess(process, keepProcess);
