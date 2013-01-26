e0be68ccec52c9c653bee877d3974494d75a9b1b
John Leacox <johnlcox@gmail.com>
2013-01-25 21:23:13 -0600

    Add FinalizedProcess method javadoc

--- src/main/java/com/leacox/process/FinalizedProcess.java
+++ src/main/java/com/leacox/process/FinalizedProcess.java
@@ -63,27 +63,110 @@ public class FinalizedProcess implements Closeable {
 		this.keepProcess = keepProcess;
 	}
 
+	/**
+	 * Kills the subprocess. The subprocess represented by this
+	 * {@code FinalizedProcess} object is forcibly terminated.
+	 */
 	public void destroy() {
 		process.destroy();
 	}
 
+	/**
+	 * Returns the exit value for the subprocess.
+	 * 
+	 * @return the exit value of the subprocess represented by this
+	 *         {@code FinalizedProcess} object. By convention, the value
+	 *         {@code 0} indicates normal termination.
+	 * @throws IllegalThreadStateException
+	 *             if the subprocess represented by this
+	 *             {@code FinalizedProcess} object has not yet terminated
+	 */
 	public int exitValue() {
 		return process.exitValue();
 	}
 
+	/**
+	 * Returns the input stream connected to the error output of the subprocess.
+	 * The stream obtains data piped from the error output of the process
+	 * represented by this {@code FinalizedProcess} object.
+	 * 
+	 * <p>
+	 * If the standard error of the subprocess has been redirected using
+	 * {@link FinalizedProcessBuilder#redirectErrorStream(boolean)} then this
+	 * method will return a null input stream</a>.
+	 * 
+	 * <p>
+	 * Implementation note: It is a good idea for the returned input stream to
+	 * be buffered.
+	 * 
+	 * @return the input stream connected to the error output of the subprocess
+	 */
 	public InputStream getErrorStream() {
 		return process.getErrorStream();
 	}
 
+	/**
+	 * Returns the input stream connected to the normal output of the
+	 * subprocess. The stream obtains data piped from the standard output of the
+	 * process represented by this {@code FinalizedProcess} object.
+	 * 
+	 * <p>
+	 * If the standard error of the subprocess has been redirected using
+	 * {@link FinalizedProcessBuilder#redirectErrorStream(boolean)} then the
+	 * input stream returned by this method will receive the merged standard
+	 * output and the standard error of the subprocess.
+	 * 
+	 * <p>
+	 * Implementation note: It is a good idea for the returned input stream to
+	 * be buffered.
+	 * 
+	 * @return the input stream connected to the normal output of the subprocess
+	 */
 	public InputStream getInputStream() {
 		return process.getInputStream();
 	}
 
+	/**
+	 * Returns the output stream connected to the normal input of the
+	 * subprocess. Output to the stream is piped into the standard input of the
+	 * process represented by this {@code FinalizedProcess} object.
+	 * 
+	 * <p>
+	 * Implementation note: It is a good idea for the returned output stream to
+	 * be buffered.
+	 * 
+	 * @return the output stream connected to the normal input of the subprocess
+	 */
 	public OutputStream getOutputStream() {
 		return process.getOutputStream();
 	}
 
+	/**
+	 * Causes the current thread to wait, if necessary, until the process
+	 * represented by this {@code FinalizedProcess} object has terminated. This
+	 * method returns immediately if the subprocess has already terminated. If
+	 * the subprocess has not yet terminated, the calling thread will be blocked
+	 * until the subprocess exits. If the subprocess execution takes longer than
+	 * the specified {@code timeoutMilliseconds}, then the blocked thread will
+	 * be interrupted.
+	 * 
+	 * @param timeoutMilliseconds
+	 *            time, in milliseconds, to wait on the subprocess blocking
+	 *            thread before timing out. (must be greater than 0)
+	 * @return the exit value of the subprocess represented by this
+	 *         {@code Process} object. By convention, the value {@code 0}
+	 *         indicates normal termination.
+	 * @throws IllegalArgumentException
+	 *             if timeoutMilliseconds is negative or zero.
+	 * @throws InterruptedException
+	 *             if the subprocess execution times out or the current thread
+	 *             is interrupted by another thread while it is waiting.
+	 */
 	public int waitFor(int timeoutMilliseconds) throws InterruptedException {
+		if (timeoutMilliseconds <= 0) {
+			throw new IllegalArgumentException("timeoutMilliseconds: <= 0");
+		}
+
 		Timer timer = null;
 		try {
 			timer = new Timer(true);
